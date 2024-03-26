789a7a47eea04215fda267e73e03883a68b465fb
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-03-26 10:10:39 +0100

    ajout commentaires

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 from dokusan import generators
 
+# Permet de convertir de string en int
 def convertir_grille_dokusan(grille_str):
     grille = []
     lignes = [grille_str[i:i+9] for i in range(0, len(grille_str), 9)]
@@ -44,19 +45,21 @@ def check_column(list_full):
 #Check le carré
 def check_carre(list_full):
     list_carre = []
-
+    # On isole chaque carré 3x3
     for ligne_full in range(0, 9, 3):
         for colonne_full in range(0, 9, 3):
             carre = []
+            # On parcours les 3 lignes et colonnes de chaque carré
             for row in range(ligne_full, ligne_full + 3):
                 for col in range(colonne_full, colonne_full + 3):
                     carre.append(list_full[row][col])
             list_carre.append(carre)
+            # On vérifie que le carré est bien unique
             if len(carre) != len(set(carre)):
                 return False
     return True
 
-#Vérifie si la grille du sudoku est valide
+#Vérifie si la grille du sudoku est valide avec les 3 fonctions créées précédemment
 def check_sudoku(list_full):
     return check_column(list_full) and check_carre(list_full) and check_row(list_full)
 
@@ -67,13 +70,13 @@ def afficher(data):
             print(data[i][j], end=" ")
         print()
 
-#Trouve le zero sur la grille
+#Trouve le zero en parcourant la grille
 def find_zero(data):
     for i in range(9):
         for j in range(9):
             if data[i][j] == 0:
                 return i, j
-    return None, None
+    return None, None # Si il n'y a plus de 0, renvoie None
 
 #Check les numéros sur les lignes, colonnes et carrés pour savoir quelle chiffre il peut mettre
 def valid_move(data, row, col, num):
@@ -87,26 +90,26 @@ def valid_move(data, row, col, num):
         if data[i][col] == num:
             return False
 
-    # Vérifie le carré
+    # Vérifie le carré en soustrayant le reste de la division par 3
     startRow = row - row % 3
     startCol = col - col % 3
     for i in range(3):
         for j in range(3):
             if data[i + startRow][j + startCol] == num:
                 return False
-
     return True
 
+# Permet de résoudre le sudoku en trouvant le zero, puis en checkant où il peut mettre le numero
 def solve_sudoku(data):
     row, col = find_zero(data)
-    if row == None:
+    if row == None: # Si None alors la grille est résolu
         return True
     for num in range(1, 10):
         if valid_move(data, row, col, num):
             data[row][col] = num
             if solve_sudoku(data):
                 return True
-            data[row][col] = 0
+            data[row][col] = 0 # Si pas de solution valide, alors remet 0 et essaye le numero suivant avec la récursivité
     return False
 
 def choice_difficulty():
@@ -117,12 +120,13 @@ def choice_difficulty():
 
     choix = input("Entrez le numéro correspondant à votre choix (1, 2 ou 3): ")
 
+    # Renvoie différentes grilles avec un niveau différent suivant le choix saisis
     if choix == '1':
         grille_difficulty = generators.random_sudoku(avg_rank=50)
     elif choix == '2':
         grille_difficulty = generators.random_sudoku(avg_rank=150)
     elif choix == '3':
-        grille_difficulty = generators.random_sudoku(avg_rank=250)
+        grille_difficulty = generators.random_sudoku(avg_rank=350)
     else:
         print("Saisie incorrecte. Veuillez réessayer.")
         return choice_difficulty()
